48ad58f3abe4d374856c4cdb6607c40a1cacc6df
Vj <Vijayanand.Chinnakannan@hotmail.com>
2021-02-11 20:19:53 -0500

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Vijayanand Chinnakannan on 2021-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
